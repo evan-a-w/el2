20a40b34859ec55ab653711bc13f31407a66772e
Evan Williams <evanawilliams@live.com>
2023-07-10 00:55:30 +1000

    😊

--- lib/pratt.ml
+++ lib/pratt.ml
@@ -24,29 +24,52 @@ module Trie = struct
   end)
 
   type node = {
-    is_end : bool;
     children : node Char.Table.t;
     matches : match_info Match_table.t;
   }
+
+  let create_node () =
+    { children = Char.Table.create (); matches = Match_table.create () }
 
   type t = { children : node Char.Table.t; mutable bp_counter : int }
 
   let empty () = { children = Char.Table.create (); bp_counter = 100 }
 
   let get_bp t ~operator_type ~decrement_bp =
-    let dec = match decrement_bp, operator_type with false, _ -> 0 | _, Operator_type.Unary -> 1 | _, Binary -> 2 in
+    let dec =
+      match (decrement_bp, operator_type) with
+      | false, _ -> 0
+      | _, Operator_type.Unary -> 1
+      | _, Binary -> 2
+    in
     let res = t.bp_counter in
     t.bp_counter <- t.bp_counter - dec;
     res
 
-  let insert ?(associativity=Associativity.Left) t  ~operator_type ~match_type ~operator ~decrement_bp =
+  let insert ?(associativity = Associativity.Left) t ~operator_type ~match_type
+      ~operator ~decrement_bp =
     let rec insert_node node ~chars =
       match Sequence.next chars with
-      | None -> (
-          Match_table.add_exn node.matches ~key:(operator_type, match_type)
-            ~data:{ binding_power = get_bp t ~operator_type ~decrement_bp; associativity };
-      )
+      | None ->
+          Match_table.add_exn node.matches
+            ~key:(operator_type, match_type)
+            ~data:
+              {
+                binding_power = get_bp t ~operator_type ~decrement_bp;
+                associativity;
+              }
+      | Some (c, chars) ->
+          let child =
+            Char.Table.find_or_add node.children c ~default:create_node
+          in
+          insert_node child ~chars
     in
+    let chars = String.to_list operator |> Sequence.of_list in
+    match Sequence.next chars with
+    | None -> ()
+    | Some (c, chars) ->
+        let child = Char.Table.find_or_add t.children c ~default:create_node in
+        insert_node child ~chars
 end
 
 (* (left, right) *)
